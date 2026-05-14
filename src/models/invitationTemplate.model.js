e27944fdd6c63149242d9314689c4b8f2af73f03
Nguyen Thien An <an.nguyenthien112802@gmail.com>
2026-05-14 17:21:45 +0700

    update

--- src/models/invitationTemplate.model.js
+++ src/models/invitationTemplate.model.js
@@ -18,7 +18,7 @@ const invitationTemplateSchema = new mongoose.Schema({
     },
     isActive: { 
         type: Boolean, 
-        default: true 
+        default: false 
     },
     loveGiftsButton: {
         type: {
